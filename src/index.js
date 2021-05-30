bc9904c55600f7137eef04692edf39ca476828ec
charlenebatiste <batistecharlene@gmail.com>
2021-05-30 16:24:20 -0400

    track game history

--- src/index.js
+++ src/index.js
@@ -23,52 +23,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-	// In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component classes that have a constructor should start with a super(props) call.
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		}
-	}
-
-	handleClick(i) {
-		const squares = this.state.squares.slice();
-		// we call .slice() to create a copy of the squares array to modify instead of modifying the existing array.
-		if (calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({
-			squares: squares,
-			xIsNext: !this.state.xIsNext,
-			// Each time a player moves, xIsNext (a boolean) will be flipped to determine which player goes next and the game’s state will be saved. 
-		});
-	}
-
 	renderSquare(i) {
 		return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
 
 	render() {
-		const winner = calculateWinner(this.state.squares);
-		let status;
-		if (winner) {
-			status = "Winner: " + winner;
-		} else {
-			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-		}
-
 		return (
 			<div>
-				<div className="status">
-					{status}
-				</div>
 				<div className="board-row">
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -90,20 +56,92 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [{
+				squares: Array(9).fill(null),
+			}],
+			stepNumber: 0,
+			xIsNext: true,
+		};
+	}
+
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+			history: history.concat([{
+				squares: squares
+			}]),
+			stepNumber: history.length,
+			xIsNext: !this.state.xIsNext,
+		});
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0,
+		});
+	}
+
 	render() {
+		const history = this.state.history;
+		const current = history[this.state.stepNumber];
+		const winner = calculateWinner(current.squares);
+
+		const moves = history.map((step, move) => {
+			const desc = move ?
+				'Go to move #' + move :
+				'Go to game start';
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			);
+		});
+
+		let status;
+		if (winner) {
+			status = 'Winner: ' + winner;
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		}
+
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board />
-				</div>{" "}
+					<Board
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
+				</div>
 				<div className="game-info">
-					<div> {/* status */} </div>{" "}
-					<ol> {/* TODO */} </ol>{" "}
-				</div>{" "}
+					<div> {status} </div>
+					<ol> {moves} </ol>
+				</div>
 			</div>
 		);
 	}
 }
+
+
+
+// **** To collect data from multiple children, or to have two child components communicate with each other, you need to declare the shared state in their parent component instead. The parent component can pass the state back down to the children by using props; this keeps the child components in sync with each other and with the parent component. ****
+
+// ========================================
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById("root")
+);
+
 
 function calculateWinner(squares) {
 	const lines = [
@@ -124,12 +162,3 @@ function calculateWinner(squares) {
 	}
 	return null;
 }
-
-// **** To collect data from multiple children, or to have two child components communicate with each other, you need to declare the shared state in their parent component instead. The parent component can pass the state back down to the children by using props; this keeps the child components in sync with each other and with the parent component. ****
-
-// ========================================
-
-ReactDOM.render(
-	<Game />,
-	document.getElementById("root")
-);
